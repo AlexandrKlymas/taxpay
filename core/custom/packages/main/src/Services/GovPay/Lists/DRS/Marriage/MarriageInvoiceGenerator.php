6ceff4c5349dc298d3084a37d4ce90be1dd15097
KlymAlex <alexandrkymas@dda.digital>
2022-02-21 22:17:06 +0200

    new (Marriage Invoice): created Generator and views

--- core/custom/packages/main/src/Services/GovPay/Lists/DRS/Marriage/MarriageInvoiceGenerator.php
+++ core/custom/packages/main/src/Services/GovPay/Lists/DRS/Marriage/MarriageInvoiceGenerator.php
@@ -7,23 +7,16 @@ use EvolutionCMS\Main\Services\GovPay\Models\ServiceOrder;
 
 class MarriageInvoiceGenerator extends BaseInvoiceGenerator
 {
-    public function generate(ServiceOrder $serviceOrder)
+    public function generate(ServiceOrder $serviceOrder):string
     {
         $invoices = '';
         $recipients = $serviceOrder->mainRecipients;
 
-        evo()->invokeEvent('OnCheckFound',[
-            'service_order'=>$serviceOrder,
+        $invoices .= \View::make('partials.services.invoices.marriage_invoice')->with([
+            'recipients'=>$recipients,
+            'order' => $serviceOrder
         ]);
 
-        foreach ($recipients as $recipient) {
-
-            $invoices .= \View::make('partials.services.invoices.pcr_invoice')->with([
-                'recipient'=>$recipient,
-                'order' => $serviceOrder
-            ]);
-
-        }
         return \View::make('partials.services.invoices.owner',[
             'invoices' => $invoices,
         ])->render();
