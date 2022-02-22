7fcc8056fa364f2cddce42dab177d97d56095b43
KlymAlex <alexandrkymas@dda.digital>
2022-02-22 23:53:22 +0200

    new (Base Invoice Gen): init with DI serviceFactory

--- core/custom/packages/main/src/Services/GovPay/Lists/BaseService/BaseInvoiceGenerator.php
+++ core/custom/packages/main/src/Services/GovPay/Lists/BaseService/BaseInvoiceGenerator.php
@@ -3,10 +3,18 @@
 namespace EvolutionCMS\Main\Services\GovPay\Lists\BaseService;
 
 use EvolutionCMS\Main\Services\GovPay\Contracts\Service\IInvoiceGenerator;
+use EvolutionCMS\Main\Services\GovPay\Contracts\Service\IServiceFactory;
 use EvolutionCMS\Main\Services\GovPay\Models\ServiceOrder;
 
 class BaseInvoiceGenerator implements IInvoiceGenerator
 {
+    protected IServiceFactory $serviceFactory;
+
+    public function __construct(IServiceFactory $serviceFactory)
+    {
+        $this->serviceFactory = $serviceFactory;
+    }
+
     public function generate(ServiceOrder $serviceOrder):string
     {
         $invoices = '';
