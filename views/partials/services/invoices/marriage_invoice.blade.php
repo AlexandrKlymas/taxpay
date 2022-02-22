17efa284db27c2997d6bb5db97e785fdda755177
KlymAlex <alexandrkymas@dda.digital>
2022-02-22 23:54:18 +0200

    fix (Invoice view with DecoratorList): create view  with Deco

--- views/partials/services/invoices/marriage_invoice.blade.php
+++ views/partials/services/invoices/marriage_invoice.blade.php
@@ -1,19 +1,14 @@
-@php
-    /** @var $order \EvolutionCMS\Main\Services\GovPay\Models\ServiceOrder */
-    /** @var $recipients array */
-    /** @var $recipient \EvolutionCMS\Main\Services\GovPay\Models\PaymentRecipient */
-@endphp
-
 <div class="payment_table">
     <div style="padding-bottom:5px;text-align:center;"><img src="@include('partials.services.logo')" alt=""></div>
-    @foreach($recipients as $recipient)
+    @foreach($recipients->getRecipientList() as $recipient)
         <div class="table">
-            <div style="padding-bottom:15px;text-align:center;">
+            <div style="padding-bottom:15px;text-align:center;"></div>
             <table align="center" width="750px" cellpadding="5" cellspacing="7">
                 <tbody>
                 <tr>
                     <td>Квитанція ID</td>
-                    <td class="data"><b>{{ \EvolutionCMS\Main\Support\Helpers::formattedCheckId($recipient->check_id) }}</b></td>
+                    <td class="data">
+                        <b>{{ \EvolutionCMS\Main\Support\Helpers::formattedCheckId($recipient->getCheckId()) }}</b></td>
                     <td>Дата оплати</td>
                     <td class="data"><b>{{ $order->liqpay_payment_date }}</b></td>
                 </tr>
@@ -42,32 +37,40 @@
                     <td colspan="2">Банк одержувача</td>
                 </tr>
                 <tr>
-                    <td colspan="2" class="data"><b>{{ $recipient->recipient_name }}</b></td>
-                    <td colspan="2" class="data"><b>{{ $recipient->recipient_bank_name }}</b></td>
+                    <td colspan="2" class="data"><b>{{ $recipient->getRecipient()->recipient_name }}</b></td>
+                    <td colspan="2" class="data"><b>{{ $recipient->getRecipientBankName() }}</b></td>
                 </tr>
                 <tr>
-                    <td colspan="3">Рахунок  IBAN</td>
+                    <td colspan="3">Рахунок IBAN</td>
                     <td>ЄДРПОУ</td>
                 </tr>
                 <tr>
-                    <td colspan="3" class="data"><b>{{ $recipient->account }}</b></td>
-                    <td class="data"><b>{{ $recipient->edrpou }}</b></td>
+                    <td colspan="3" class="data"><b>{{ $recipient->getRecipient()->iban }}</b></td>
+                    <td class="data"><b>{{ $recipient->getRecipient()->edrpou }}</b></td>
                 </tr>
                 <tr>
                     <td><i>Призначення платежу</i></td>
-                    <td colspan="3" class="data"><b>{{ $recipient->purpose }}</b></td>
+                    <td colspan="3" class="data">
+                        <b>{{\EvolutionCMS\Main\Services\GovPay\Support\PurposeHelpers::parse($recipient->getRecipient()->purpose_template,$order->form_data) }}</b>
+                    </td>
                 </tr>
                 <tr>
                     <td><i>Сума</i></td>
-                    <td colspan="3" class="data"><b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($order->sum) }}</b></td>
+                    <td colspan="3" class="data">
+                        <b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($recipient->getRecipient()->sum) }}</b>
+                    </td>
                 </tr>
                 <tr>
                     <td><i>Сервісний збір</i></td>
-                    <td colspan="3" class="data"><b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($order->service_fee) }}</b></td>
+                    <td colspan="3" class="data">
+                        <b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($recipient->getGPCommission()) }}</b>
+                    </td>
                 </tr>
                 <tr>
                     <td><i>Всього сплачено</i></td>
-                    <td colspan="3" class="data"><b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($order->total) }}</b></td>
+                    <td colspan="3" class="data">
+                        <b>{{ \EvolutionCMS\Main\Services\GovPay\Support\AmountHelpers::amountInWord($recipient->getRecipient()->sum + $recipient->getGPCommission()) }}</b>
+                    </td>
                 </tr>
                 </tbody>
             </table>
