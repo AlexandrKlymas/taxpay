167a170a50e9d43bd9a5807cabab466e24f5e5eb
KlymAlex <alexandrkymas@dda.digital>
2022-02-23 18:50:59 +0200

    fix (BankExport PaySender): crop recipient name and recipient purpose

--- core/custom/packages/main/src/Services/PaymentsToBankSender/PaymentsSender.php
+++ core/custom/packages/main/src/Services/PaymentsToBankSender/PaymentsSender.php
@@ -2,59 +2,64 @@
 
 namespace EvolutionCMS\Main\Services\PaymentsToBankSender;
 
-
+use DocumentParser;
 use EvolutionCMS\Main\Services\GovPay\Models\PaymentRecipient;
 use EvolutionCMS\Main\Services\PaymentsToBankSender\Exceptions\FileNotPutException;
 use EvolutionCMS\Main\Services\PaymentsToBankSender\Repository\BankTKFtpRepository;
 use EvolutionCMS\Main\Services\PaymentsToBankSender\Repository\PaymentSenderExportFileRepository;
+use Exception;
+use function EvolutionCMS;
 
 class PaymentsSender
 {
-
     /**
      * @var PaymentSenderExportFileRepository
      */
-    private $fileRepository;
+    private PaymentSenderExportFileRepository $fileRepository;
     /**
      * @var BankTKFtpRepository
      */
-    private $bankRepository;
+    private BankTKFtpRepository $bankRepository;
     /**
-     * @var \DocumentParser
+     * @var DocumentParser
      */
-    private $evo;
+    private DocumentParser $evo;
 
     public function __construct()
     {
         $this->fileRepository = new PaymentSenderExportFileRepository(time());
         $this->bankRepository = new BankTKFtpRepository();
-        $this->evo = \EvolutionCMS();
+        $this->evo = EvolutionCMS();
     }
 
-    private $transactionsFieldWithSizes = [
-        'mfoA' => 9, // Код банка (МФО) А
-        'accountA' => 34, // Личный счет клиента банка А *
-        'mfoB' => 9, // Код банка (МФО) банка Б
-        'accountB' => 34, // Личный счет клиента банка Б *
-        'debetcredit' => 1, // Флажок  "дебет/кредит" платежа
-        'summ' => 16, // Сума платежа
-        'doctype' => 2, // Вид документа
-        'op_platezh_nomer' => 10, // Операционный номер платежа
-        'kod_valuty' => 3, // Код валюты платежа
-        'date_file' => 6, // Дата формирования платежного файла ГГММДД
-        'date_platezh' => 6, // Дата осуществления платежа ГГММДД
-        'nameA' => 38, // Название плательщика (клиента А)
-        'nameB' => 38, // Название получателя (клиента Б)
-        'naznachenie_platezh' => 160, // Назначение платежа*
-        'add_rekvizit' => 60, // Дополнительные реквизиты
-        'kod_naznachenie_platezh' => 3, // Код назначения платежа
-        'bis' => 2, // Количество строк БИС (блока информационных строк)
-        'idA' => 14, // Ідентифікатор клієнта А
-        'idB' => 14 // Идентификатор клиента Б *
+    private array $transactionsFieldWithSizes = [
+        'mfoA' => 9, // Код банка (МФО) А 1 – 9 (с 1 по 9 строку)
+        'accountA' => 34, // Личный счет клиента банка А * 10 – 43
+        'mfoB' => 9, // Код банка (МФО) банка Б 44 – 52
+        'accountB' => 34, // Личный счет клиента банка Б * 53 – 86
+        'debetcredit' => 1, // Флажок  "дебет/кредит" платежа 87 – 87
+        'summ' => 16, // Сума платежа 88 – 103
+        'doctype' => 2, // Вид документа 104 – 105
+        'op_platezh_nomer' => 10, // Операционный номер платежа 106 – 115
+        'kod_valuty' => 3, // Код валюты платежа 116 – 118
+        'date_file' => 6, // Дата формирования платежного файла ГГММДД 119 – 124
+        'date_platezh' => 6, // Дата осуществления платежа ГГММДД 125 – 130
+        'nameA' => 38, // Название плательщика (клиента А) 131 – 168
+        'nameB' => 38, // Название получателя (клиента Б) 169 – 206
+        'naznachenie_platezh' => 160, // Назначение платежа* 207 – 366
+        'add_rekvizit' => 60, // Дополнительные реквизиты  367 – 426
+        'kod_naznachenie_platezh' => 3, // Код назначения платежа  427 – 429
+        'bis' => 2, // Количество строк БИС (блока информационных строк) 430 – 431
+        'idA' => 14, // Ідентифікатор клієнта А 432 – 445
+        'idB' => 14 // Идентификатор клиента Б * 446 – 459
     ];
 
-
-    public function sendConfirmedPaymentsToBank()
+    /**
+     * @throws \PHPMailer\PHPMailer\Exception
+     * @throws FileNotPutException
+     * @throws Exception
+     */
+    public function sendConfirmedPaymentsToBank(): bool
     {
         $recipients = PaymentRecipient::getConfirmedPayments();
 
@@ -71,57 +76,59 @@ class PaymentsSender
 
         $transactionFileName = $this->fileRepository->getTransactionFileName();
 
-
         $transactionFile = $this->fileRepository->createTransactionFile($transactionStrings);
 
-
         try {
-            $this->bankRepository->sendFileToBank($transactionFile, "/IN/GOSPAY/" . $transactionFileName, true);
+            $this->bankRepository->sendFileToBank($transactionFile, "/IN/GOSPAY/"
+                . $transactionFileName, true);
 
             foreach ($recipients as $recipient) {
                 $recipient->changeStatusToFinished();
             }
 
             $this->fileRepository->archiveFile($transactionFileName);
-            $this->evo->logEvent(610, 1, 'Файл ' . $transactionFileName . ' отправлен успешно', 'Файл ' . $transactionFileName . ' отправлен успешно');
+            $this->evo->logEvent(610, 1, 'Файл ' . $transactionFileName
+                . ' отправлен успешно', 'Файл ' . $transactionFileName . ' отправлен успешно');
 
         } catch (FileNotPutException $e) {
             $this->fileRepository->moveFileToBad($transactionFileName);
-            $this->evo->logEvent(611, 2, 'Файл ' . $transactionFileName . ' не отправлен', 'Файл ' . $transactionFileName . ' не отправлен');
+            $this->evo->logEvent(611, 2, 'Файл ' . $transactionFileName
+                . ' не отправлен', 'Файл ' . $transactionFileName . ' не отправлен');
             throw $e;
         }
 
         return true;
     }
 
-    private function createTransactions($recipients)
+    private function createTransactions($recipients): array
     {
         $transactions = [];
-
 
         /** @var PaymentRecipient $recipient */
         foreach ($recipients as $recipient) {
 
             $sum = $recipient->amount * 100;
 
-            $datePlatezh = $recipient->serviceOrder->liqpay_payment_date ? $recipient->serviceOrder->liqpay_payment_date->format('ymd') : date('ymd');
+            $paymentDate = date('ymd');
 
+            if (!empty($recipient->serviceOrder->liqpay_payment_date)) {
+                $paymentDate = $recipient->serviceOrder->liqpay_payment_date->format('ymd');
+            }
 
             $serviceOrderId = $recipient->serviceOrder->id;;
-            $date = date('ymd');
 
+            $date = date('ymd');
 
             switch ($recipient->recipient_type) {
                 case PaymentRecipient::RECIPIENT_TK_COMMISSION:
-                    $opPlatezhNomer = 'KB-' . $serviceOrderId;
+                    $operationNumber = 'KB-' . $serviceOrderId;
                     break;
                 case PaymentRecipient::RECIPIENT_GOVPAY_PROFIT:
-                    $opPlatezhNomer = 'KH-' . $serviceOrderId;
+                    $operationNumber = 'KH-' . $serviceOrderId;
                     break;
                 default:
-                    $opPlatezhNomer = $serviceOrderId;
+                    $operationNumber = $serviceOrderId;
             }
-
 
             $transactions[] = [
                 'mfoA' => '380106',
@@ -131,30 +138,29 @@ class PaymentsSender
                 'debetcredit' => '1',
                 'summ' => $sum,
                 'doctype' => $recipient->isMainRecipient() ? 14 : 2,
-                'op_platezh_nomer' => $opPlatezhNomer,
+                'op_platezh_nomer' => $operationNumber,
                 'kod_valuty' => '980',
                 'date_file' => $date,
-                'date_platezh' => $datePlatezh,
-                'nameA' => str_pad(' ', 38, " ", STR_PAD_LEFT),
-                'nameB' => $recipient->recipient_name,
+                'date_platezh' => $paymentDate,
+                'nameA' => str_pad(' ', $this->transactionsFieldWithSizes['nameA'], ' ', STR_PAD_LEFT),
+                'nameB' => mb_substr($recipient->recipient_name, 0, $this->transactionsFieldWithSizes['nameB']),
                 'naznachenie_platezh' => $recipient->purpose,
                 'add_rekvizit' => '#u' . $recipient->serviceOrder->liqpay_transaction_id,
-                'kod_naznachenie_platezh' => str_pad(' ', 3, " ", STR_PAD_LEFT),
+                'kod_naznachenie_platezh' => str_pad(' ', $this->transactionsFieldWithSizes['kod_naznachenie_platezh'], ' ', STR_PAD_LEFT),
                 'bis' => '00',
-                'idA' => str_pad(' ', 14, " ", STR_PAD_LEFT),
+                'idA' => str_pad(' ', $this->transactionsFieldWithSizes['idA'], " ", STR_PAD_LEFT),
                 'idB' => $recipient->edrpou
             ];
         }
 
-
         return $transactions;
     }
 
-    private function convertTransactionsToStrings(array $transactions)
+    private function convertTransactionsToStrings(array $transactions): string
     {
         $strings = '';
         foreach ($transactions as $transaction) {
-            $transaction = $this->addSpaceToTransactioFields($transaction);
+            $transaction = $this->addSpaceToTransactionFields($transaction);
             $strings .= iconv("UTF-8", "cp1251",
                     implode('', $transaction))
                 . chr(13) . chr(10);
@@ -163,7 +169,7 @@ class PaymentsSender
         return $strings;
     }
 
-    private function addSpaceToTransactioFields($transaction)
+    private function addSpaceToTransactionFields($transaction): array
     {
         //дописываем нужное количество пробелов спереди
         $tmp = [];
@@ -178,20 +184,19 @@ class PaymentsSender
 
         $realNameBLength = mb_strlen($transaction['nameB'], "UTF-8");
         $lengthDiff = $this->transactionsFieldWithSizes['nameB'] - $realNameBLength;
-        if ($lengthDiff<0) {
+        if ($lengthDiff < 0) {
 
-            $newNameALength = $this->transactionsFieldWithSizes['nameB']-($lengthDiff*-1);
-            if($newNameALength>2){
-                $transaction['nameA'] = substr($transaction['nameA'],0,$newNameALength);
+            $newNameALength = $this->transactionsFieldWithSizes['nameB'] - ($lengthDiff * -1);
+            if ($newNameALength > 2) {
+                $transaction['nameA'] = substr($transaction['nameA'], 0, $newNameALength);
             }
         }
 
         return $transaction;
     }
 
-    private function mdStrPad($input, $pad_length, $pad_string, $pad_style, $encoding = "UTF-8")
+    private function mdStrPad(string $input, int $padLength, string $padString, string $padStyle, string $encoding = "UTF-8"): string
     {
-        $out = str_pad($input, strlen($input) - mb_strlen($input, $encoding) + $pad_length, $pad_string, $pad_style);
-        return $out;
+        return str_pad($input, strlen($input) - mb_strlen($input, $encoding) + $padLength, $padString, $padStyle);
     }
 }
