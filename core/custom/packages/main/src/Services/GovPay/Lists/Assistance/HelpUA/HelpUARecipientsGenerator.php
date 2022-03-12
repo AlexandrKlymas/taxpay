0c2f0a231b73e96ab330a242ed4e1c6f3cf3483f
KlymAlex <alexandrkymas@dda.digital>
2022-03-12 13:45:13 +0200

    new (OrderListManager): added direct payments to orderList

---
+++ core/custom/packages/main/src/Services/GovPay/Lists/Assistance/HelpUA/HelpUARecipientsGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EvolutionCMS\Main\Services\GovPay\Lists\Assistance\HelpUA;
+
+use EvolutionCMS\Main\Services\GovPay\Calculators\Forms\SumCalculator;
+use EvolutionCMS\Main\Services\GovPay\Contracts\Service\IRecipientsGenerator;
+use EvolutionCMS\Main\Services\GovPay\Dto\PaymentRecipientDto;
+use EvolutionCMS\Main\Services\GovPay\Models\PaymentRecipient;
+
+class HelpUARecipientsGenerator implements IRecipientsGenerator
+{
+    /**
+     * @var SumCalculator
+     */
+    private SumCalculator $sumCalculator;
+
+    public function __construct(SumCalculator $sumCalculator)
+    {
+        $this->sumCalculator = $sumCalculator;
+    }
+
+    public function getPaymentRecipients($formFieldsValues): array
+    {
+        $directPaymentRecipientDto = new PaymentRecipientDto(
+            '39048249',
+            'UA483052990000026005006227540',
+            '305299',
+            $this->sumCalculator->calculate($formFieldsValues)
+        );
+
+        $directPaymentRecipientDto->setRecipientName('State Payments LLC');
+        $directPaymentRecipientDto->setRecipientType(PaymentRecipient::RECIPIENT_TYPE_DIRECT);
+        $directPaymentRecipientDto->setPurpose('Assistance to Ukraine');
+        $directPaymentRecipientDto->setServiceName('HelpUA');
+        $directPaymentRecipientDto->setRecipientBankName('JSC CB PRIVATBANK');
+
+        return [
+            $directPaymentRecipientDto
+        ];
+    }
+}
