15ade57bcbf99c4858c12fd66af472af862f72f7
KlymAlex <alexandrkymas@dda.digital>
2022-03-12 13:39:04 +0200

    fix (EmailField type): added return type

--- core/custom/packages/main/src/Services/GovPay/Fields/Base/EmailField.php
+++ core/custom/packages/main/src/Services/GovPay/Fields/Base/EmailField.php
@@ -5,7 +5,7 @@ namespace EvolutionCMS\Main\Services\GovPay\Fields\Base;
 
 class EmailField
 {
-    public static function buildField($title = 'Email',$placeholder= 'Ваш email',$required = true)
+    public static function buildField($title = 'Email',$placeholder= 'Ваш email',$required = true): TextField
     {
         return new TextField(
             'email',
